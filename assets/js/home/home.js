05562eed22eb908535a5c58c67f9d9a871dee12f
Wagner Luz <wagnnersousa@hotmail.com>
2021-04-05 12:31:49 -0300

    Corrigido enter dos inputs que não estavam atualizando

--- assets/js/home/home.js
+++ assets/js/home/home.js
@@ -1,14 +1,19 @@
 function btnSearchVideos(){
 
-	var url  = URL + "/videos/get&search=" + $('#input-search').val()+"&"+$(".form-durations").serialize();
-
 	$('#input-search, .input-day').on('keypress', function (e) {
+		
         if(e.which === 13 && $("#input-search").val()){
+
+        	var url  = URL + "/videos/get&search=" + $('#input-search').val()+"&"+$(".form-durations").serialize();
+
         	window.location = url;
         }
    	});
 
 	$('.btn-search').on('click', function (e) {
+
+		var url  = URL + "/videos/get&search=" + $('#input-search').val()+"&"+$(".form-durations").serialize();
+
 		window.location = url;
 	});
 
